0d44b6e984a060d56bb52476e9e02b42e9effcfa
sunhaoqi <hockeysun86@gmail.com>
2017-04-29 19:41:28 +0800

    implemented HMC for linear regression

--- linear_regression.py
+++ linear_regression.py
@@ -1,14 +1,96 @@
 """
-Linear regression using neural sampling
+Linear regression using neural sampling.
 """
-
 import numpy as np
+from sklearn.base import BaseEstimator, RegressorMixin
+from sklearn.utils import check_array, check_X_y, check_random_state
 #import pymc3 as pm
+import matplotlib.pyplot as plt
+import seaborn as sb
+sb.set_style('ticks')
 from neural_sampler import *
+
+
+class LinearRegressionNS(BaseEstimator, RegressorMixin):
+    def __init__(self, sigma2_y=1., sigma2_W=1., fit_intercept=True, tau=10.,
+                n_draw=1000, n_iter=100, n_chain=1, keep_sampling=0.5,
+                random_state=None):
+        self.sigma2_y = sigma2_y
+        self.sigma2_W = sigma2_W
+        self.fit_intercept = fit_intercept
+        self.tau = tau
+        self.n_draw = n_draw
+        self.n_iter = n_iter
+        self.n_chain = n_chain   # preserved for future use
+        self.keep_sampling = keep_sampling
+        self.random_state = random_state
+        
+    def _upost_params(self, X, y):
+        return (np.dot(X.T,X)/self.sigma2_y+np.eye(self.D)/self.sigma2_W, np.dot(X.T,y)/self.sigma2_y)
+    
+    def _dW_log_upost(self, W, use_params, *params):
+        """
+        d/dW[log(P(D|W)P(W))] = -AW+B
+        = -(sigma^-2_y*X.T*X+sigma^-2_W*I)*W+sigma^-2_y*X.T*y
+        """
+        if use_params:
+            A, B = params
+        else:
+            X, y = params
+            A, B = self._upost_params(X, y)
+        return B-np.dot(A,W)
+    
+    def _log_upost(self, W, X, y):
+        Y_XW = y-np.dot(X,W)
+        return -np.sum(Y_XW*Y_XW)/self.two_sigma2_y-np.sum(W*W)/self.two_sigma2_W
+    
+    def fit(self, X, y):
+        self.fitted_ = False
+        X_, y = check_X_y(X,y)
+        self.N = X_.shape[0]
+        if self.fit_intercept:
+            X_ = np.c_[X_, np.ones(self.N)]
+        self.D = X_.shape[1]
+        self.two_sigma2_y = 2.*self.sigma2_y
+        self.two_sigma2_W = 2.*self.sigma2_W
+        
+        self.ns = NeuralSampler(self._dW_log_upost, self.D, tau=self.tau,
+                    n_draw=self.n_draw, n_iter=self.n_iter, n_chain=self.n_chain,
+                    upost_params=self._upost_params, log_upost=self._log_upost,
+                    compute_H=True, random_state=self.random_state)
+        
+        self.Ws = self.ns.sample(X_,y)
+        self.Ws = self.Ws[int(round(self.keep_sampling*len(self.Ws))):]
+        
+        self.fitted_ = True
+        return self
 
 
 if __name__=='__main__':
     import pdb
-
-    lrns = NeuralSampler()
+    
+    # params
+    
+    random_state = 2
+    np.random.seed(random_state)
+    sigma2_y = 0.01
+    sigma2_W = 1.
+    
+    # generate data
+    
+    N = 100
+    D = 3
+    W = np.zeros(D)+[2.,1.,3.]
+    X = np.random.rand(N,D)
+    y = np.dot(X,W)+np.random.randn(N)*np.sqrt(sigma2_y)
+    
+    lrns = LinearRegressionNS(sigma2_y=sigma2_y, sigma2_W=sigma2_W,
+                fit_intercept=False, tau=100.,
+                n_draw=200, n_iter=100, n_chain=1,
+                keep_sampling=0.5, random_state=random_state)
+    
+    lrns.fit(X, y)
+    
+    plt.plot(np.array(lrns.Ws))
+    plt.show()
 
--- neural_sampler.py
+++ neural_sampler.py
@@ -9,31 +9,76 @@ from sklearn.utils import check_array, check_X_y, check_random_state
 
 class NeuralSampler(object):
     """
-    NeuralSampler(log_unnorm_posterior, n_draw=1000, n_iter=100, n_chain=1, random_state=None)
-    log_unnorm_posterior: a function to compute the log of unnorm posterior: log[P(D|theta)P(theta)]
+    NeuralSampler(log_upost, n_draw=1000, n_iter=100, n_chain=1, random_state=None)
+    dtheta_log_upost: a function to compute the log of (unnormalized) posterior: log[P(D|theta)P(theta)]
     n_draw: number of samples drawn
     n_iter: number of leapfrog steps in Hamiltonian dynamics
     n_chain: number of simultaneously sampling chains
     random_state: seed of random number generator, default None.
     """
-    def __init__(self, log_unnorm_posterior, n_draw=1000, n_iter=100, n_chain=1, random_state=None):
-        self.log_unnorm_posterior = log_unnorm_posterior
+    def __init__(self, dtheta_log_upost, len_theta, tau=10., n_draw=1000,
+            n_iter=100, n_chain=1, upost_params=None, log_upost=None,
+            compute_H=False, random_state=None):
+        self.dtheta_log_upost = dtheta_log_upost
+        self.len_theta = len_theta
+        self.tau = tau
         self.n_draw = n_draw
         self.n_iter = n_iter
         self.n_chain = n_chain   # preserved for future use
+        self.upost_params = upost_params
+        self.log_upost = log_upost
+        self.compute_H = compute_H
         self.random_state = random_state
+        self.Ms = None   # preserved for future use
 
     def sample(self, X, y=None):
         self.random_state_ = check_random_state(self.random_state)
-        X,y = check_X_y(X,y)
+        #X,y = check_X_y(X,y)
+        self.tauinv = 1./self.tau
+        self.half_tauinv = 0.5*self.tauinv
+        use_params = self.upost_params is not None
+        
+        if use_params:
+            params = self.upost_params(X,y)
+        else:
+            params = (X,y)
 
-        self.u = np.zeros()
+        theta = np.zeros(self.len_theta)
+        self.thetas = []
+        if self.compute_H:
+            self.Hs = []
 
         for i in range(self.n_draw):
+            current_theta = theta
+            current_v = self.random_state_.randn(self.len_theta)
+            
+            v = current_v + self.half_tauinv*self.dtheta_log_upost(theta, use_params, *params)
             for j in range(self.n_iter):
-                self.v = self.random_state_.normal()
-                self.v = self.v - ??
+                theta = theta + self.tauinv*v
+                if j!=self.n_iter-1:
+                    v = v + self.tauinv*self.dtheta_log_upost(theta, use_params, *params)            
+            v = v + self.half_tauinv*self.dtheta_log_upost(theta, use_params, *params)
+            #v = -v
+            
+            current_H = -self.log_upost(current_theta, X, y)+np.sum(current_v*current_v)/2.
+            proposed_H = -self.log_upost(theta, X, y)+np.sum(v*v)/2.
+            if np.log(self.random_state_.rand())<current_H-proposed_H:
+                self.thetas.append(theta)  # accept
+            else:
+                self.thetas.append(current_theta)  # reject
+            if self.compute_H:
+                self.Hs.append(-self.log_upost(theta, X, y)+np.sum(v*v)/2.)
+            
+            #v = current_v
+            #if self.compute_H:
+            #    self.Hs = []
+            #for j in range(self.n_iter):
+            #    v = v + self.half_tauinv*self.dtheta_log_upost(theta, use_params, *params)
+            #    theta = theta + self.tauinv*v
+            #    v = v + self.half_tauinv*self.dtheta_log_upost(theta, use_params, *params)
+            #    if self.compute_H:
+            #        self.Hs.append(-self.log_upost(theta, X, y)+np.sum(v*v)/2.)
+            
+        
+        return self.thetas
 
-
-if __name__=='__main__':
-    import pdb
